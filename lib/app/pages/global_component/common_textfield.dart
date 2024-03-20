95ce15a91e7035b74919a008ce9cf34a15d4753b
Fadzaa <fadza20@gmail.com>
2024-03-20 07:32:59 +0700

    feat: [FA] add login page

--- lib/app/pages/global_component/common_textfield.dart
+++ lib/app/pages/global_component/common_textfield.dart
@@ -6,52 +6,46 @@ import '../../../common/theme.dart';
 class CommonTextField extends StatelessWidget {
   CommonTextField({
     super.key,
+    required this.heading,
     required this.controller,
     required this.hintText,
-    required this.prefixIcon,
     this.isObscure,
   });
 
   TextEditingController controller;
-  String hintText, prefixIcon;
+  String hintText, heading;
   bool? isObscure;
-
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.symmetric(vertical: 10),
-      child: TextField(
-        controller: controller,
-        obscureText: isObscure ?? false,
-        // style: tsLabelLarge,
-        decoration: InputDecoration(
-          hintText: hintText,
-          // hintStyle: tsLabelLarge.copyWith(color: Colors.grey[400]),
-          filled: true,
-          fillColor: Colors.white,
-          prefixIcon: SizedBox(
-            width: 15,
-            child: Center(
-                child: SvgPicture.asset(prefixIcon)
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(heading, style: txtFormTitle,),
+
+        const SizedBox(height: 10,),
+
+        TextField(
+          controller: controller,
+          obscureText: isObscure ?? false,
+          style: txtFormTitle.copyWith(color: blackColor),
+          decoration: InputDecoration(
+            hintText: hintText,
+            hintStyle: txtFormTitle.copyWith(color: Colors.grey[400]),
+            border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(16),
+                borderSide: const BorderSide(
+                  color: Colors.grey,
+                )),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(16),
+              borderSide: const BorderSide(
+                color: primaryColor,
+              ),
             ),
           ),
-
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(10),
-            borderSide: BorderSide.none,
-          ),
-
-          focusedBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(10),
-            borderSide: const BorderSide(
-              color: primaryColor,
-            ),
-          ),
-
-
-        ),
-      ),
+        )
+      ],
     );
   }
 }
--- lib/app/pages/initial_pages/login_page/login_page_view.dart
+++ lib/app/pages/initial_pages/login_page/login_page_view.dart
@@ -1,3 +1,6 @@
+import 'package:ayamku_admin/app/pages/global_component/common_button.dart';
+import 'package:ayamku_admin/app/pages/global_component/common_textfield.dart';
+import 'package:ayamku_admin/app/router/app_pages.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import '../../../../common/theme.dart';
@@ -12,10 +15,36 @@ class LoginPageView extends GetView<LoginPageController> {
       backgroundColor: baseColor,
       body: SafeArea(
           child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+            padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 45),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
+                Text.rich(
+                  TextSpan(
+                    text: "Login \nSebagai ",
+                    style: txtHeadline1,
+                    children: [
+                      TextSpan(
+                        text: "Admin",
+                        style: txtHeadline1.copyWith(
+                            color: primaryColor
+                        ),
+                      )
+                    ]
+                  ),
+                ),
+
+                const SizedBox(height: 30,),
+
+                CommonTextField(heading: "Email" , controller: controller.emailController, hintText: "Masukkan Alamat Email"),
+
+                const SizedBox(height: 20,),
+
+                CommonTextField(heading: "Password" , controller: controller.passwordController, hintText: "Masukkan Password", isObscure: true),
+
+                const SizedBox(height: 60,),
+
+                CommonButton(text: "Login", onPressed: () => Get.toNamed(Routes.HOME_PAGE), height: 50,)
 
               ]
             ),
